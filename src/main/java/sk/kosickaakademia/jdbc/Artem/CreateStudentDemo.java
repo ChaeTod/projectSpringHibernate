e084544f538adc72bb88299d146ce4e5fc6dfbb3
Artem Kozyr <zymber6@gmail.com>
2020-11-18 10:36:13 +0100

    Added a new student to the database

--- src/main/java/sk/kosickaakademia/jdbc/Artem/CreateStudentDemo.java
+++ src/main/java/sk/kosickaakademia/jdbc/Artem/CreateStudentDemo.java
@@ -1,4 +1,38 @@
 package sk.kosickaakademia.jdbc.Artem;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class CreateStudentDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(StudentDemo.class).buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // use the session object to save Java object
+
+            // create a student object
+            System.out.println("Creating new student object...");
+            StudentDemo studentDemo = new StudentDemo("Paul", "Wall", "paul@gamail.com");
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the student object
+            System.out.println("Saving the student...");
+            session.save(studentDemo);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } finally {
+            factory.close();
+        }
+    }
 }
---
+++ src/main/java/sk/kosickaakademia/jdbc/Artem/demo/entity/StudentEntity.java
@@ -0,0 +1,88 @@
+/*
+package sk.kosickaakademia.jdbc.Artem.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student", schema = "hb_student_tracker")
+public class StudentEntity {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "first_name", nullable = true, length = 45)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name", nullable = true, length = 45)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 45)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StudentEntity that = (StudentEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentEntity{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+*/
